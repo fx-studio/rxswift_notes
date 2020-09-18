77bfc58c0a5228213e6794c1cac2c39525fab366
Fx Studio <lephuongtien.dev@gmail.com>
2020-09-18 17:21:51 +0700

    Hello Operators 2

--- Examples/BasicRxSwift/02_Operators/HelloOperators.playground/Contents.swift
+++ Examples/BasicRxSwift/02_Operators/HelloOperators.playground/Contents.swift
@@ -1,3 +1,64 @@
 import UIKit
+import RxSwift
 
-var str = "Hello, playground"
+example(of: "Hello 1") {
+    let bag = DisposeBag()
+    
+    let hello = Observable.from(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+    
+    hello
+        //.reduce("", accumulator: +)
+        .scan("", accumulator: +)
+        .subscribe(onNext: { value in
+            print(value)
+        }, onError: { error in
+            print(error)
+        }, onCompleted: {
+            print("Completed")
+        }) {
+            print("Disposed")
+        }
+        .disposed(by: bag)
+}
+
+example(of: "Hello 2") {
+    let bag = DisposeBag()
+    
+    let hello = Observable.from(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+    
+    hello
+        .map { Int($0) }
+        .subscribe(onNext: { value in
+            print(value!)
+        }, onError: { error in
+            print(error)
+        }, onCompleted: {
+            print("Completed")
+        }) {
+            print("Disposed")
+        }
+        .disposed(by: bag)
+}
+
+
+example(of: "Hello 3") {
+    let bag = DisposeBag()
+    
+    let hello = Observable.from(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+    
+    hello
+        .map { string -> Int in
+            Int(string) ?? 0
+        }
+        .filter { $0 % 2 == 0 }
+        .subscribe(onNext: { value in
+            print(value)
+        }, onError: { error in
+            print(error)
+        }, onCompleted: {
+            print("Completed")
+        }) {
+            print("Disposed")
+        }
+        .disposed(by: bag)
+}
